4fa57dc8ac7aa712d6f43f39dc446d8f0d207f34
Rogers de Pelle <rogersdepelle@gmail.com>
2018-07-12 10:59:00 -0300

    Add dev settings

--- moepex/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-from event_site import views
-
-urlpatterns = [
-    path('', views.SiteTemplateView.as_view()),
-    path('admin/', admin.site.urls),
-]
---
+++ project/settings/prod.py
@@ -0,0 +1,31 @@
+from .main import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': '',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
