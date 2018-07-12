1d2bacaf3276b8522e431d452a33982db71b8b32
Rogers de Pelle <rogersdepelle@gmail.com>
2018-07-12 13:10:17 -0300

    Add static files

--- project/settings/main.py
+++ project/settings/main.py
@@ -93,7 +93,10 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'project/static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'project/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
