b1185edac61dadf831c594671cd45ab4c84dfdb7
lfAthirson <athirsonf10@gmail.com>
2018-07-27 12:47:56 -0300

    Login

--- project/settings/main.py
+++ project/settings/main.py
@@ -101,3 +101,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = '/login/'
+LOGOUT_URL = '/logout/'
--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,8 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from event_site import views as views_event_site
 from events import views as views_events
@@ -9,6 +10,8 @@ from events import views as views_events
 urlpatterns = [
     path('', views_event_site.SiteTemplateView.as_view(), name='home'),
     path('eventos/novo', views_events.EventCreateView.as_view(), name='event-create'),
+    path('login/', auth_views.LoginView.as_view(), name="login"),
+	path('logout/', auth_views.logout, {'next_page': '/login/'}, name="logout"),
     path('admin/', admin.site.urls),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
