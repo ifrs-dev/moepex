619cde645088b4037702b3c1e122010ad383a1bf
eduardohuther <eduardocristiano2011@hotmail.com>
2018-07-27 11:58:36 -0300

    event_List

--- events/views.py
+++ events/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.views.generic import View, ListView
 
-# Create your views here.
+from events.models import Event, Registration
+from events.forms import EventForm
+
+
+class EventCreateView(CreateView):
+    model = Event
+    template_name = 'events/event-create.html'
+    form_class = EventForm
+
+
+class EventListView(ListView):
+    model = Event
+    template_name = 'event_site/index.html'
--- project/urls.py
+++ project/urls.py
@@ -3,10 +3,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from event_site import views
+from event_site import views as views_event_site
+from events import views as views_events
 
 urlpatterns = [
-    path('', views.SiteTemplateView.as_view()),
+    path('', views_event_site.SiteTemplateView.as_view(), name='home'),
+    path('eventos/novo', views_events.EventCreateView.as_view(), name='event-create'),
     path('admin/', admin.site.urls),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
