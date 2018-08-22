c90730d2cdcfddd4f523a9b90c7d7b5794ef450e
lfAthirson <athirsonf10@gmail.com>
2018-08-22 16:01:50 -0300

    Change password

--- events/urls.py
+++ events/urls.py
@@ -7,6 +7,7 @@ from events import views as views_events
 urlpatterns = [
     path('experimento/<int:pk>/', views_events.ExperimentDetailView.as_view(), name='experiment-detail'),
     path('minicurso/<int:pk>/', views_events.EventDetailView.as_view(), name='event-detail'),
+
     path('meus-eventos/', login_required(views_events.MyRegistrationsListView.as_view()), name='my-events'),
     path('experimento/novo/', login_required(views_events.ExperimentCreateView.as_view()), name='experiment-create'),
     path('minicurso/novo/', login_required(views_events.EventCreateView.as_view()), name='event-create'),
