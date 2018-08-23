6da652a70a613c4b4952416adde25fef64d9260b
Rogers de Pelle <rogersdepelle@gmail.com>
2018-08-23 11:52:19 -0300

    Adicionar banner oficial

--- project/urls.py
+++ project/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('apresentacao', views_event_site.PresentationTemplateView.as_view(), name='presentation'),
     path('login/', auth_views.LoginView.as_view(), name="login"),
     path('change_password', views_event_site.change_password, name='change_password'),
-    path('logout/', auth_views.LogoutView.as_view(), {'next_page': '/login/'}, name="logout"),
+    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
     path('signup/', views_event_site.SignUpView.as_view(), name='signup'),
     path('admin/', admin.site.urls),
     path('', include('events.urls')),
