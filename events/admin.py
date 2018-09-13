b7c1a55b801ecc8710e327522b7693126cd9cf86
Rogers de Pelle <rogersdepelle@gmail.com>
2018-09-13 11:34:29 -0300

    Adiciona filters no admin

--- events/admin.py
+++ events/admin.py
@@ -4,10 +4,16 @@ from events.models import Event, Registration, Experiment, Group
 class EventAdmin(admin.ModelAdmin):
     list_filter = ('status',)
 
+class ExperimentAdmin(admin.ModelAdmin):
+    list_filter = ('status',)
+
+class GroupAdmin(admin.ModelAdmin):
+    list_filter = ('shift',)
+
 class RegistrationAdmin(admin.ModelAdmin):
     list_filter = ('group',)
 
 admin.site.register(Event, EventAdmin)
-admin.site.register(Group)
-admin.site.register(Experiment)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Experiment, ExperimentAdmin)
 admin.site.register(Registration, RegistrationAdmin)
